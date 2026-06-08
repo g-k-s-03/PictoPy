6a323384e3980292085952327e592cd5746af562
g-k-s-03 <govindsingh97704@gmail.com>
2026-06-08 11:28:50 +0530

    chore: disable updater plugin for test release

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -247,7 +247,6 @@ fn main() {
             Some(vec!["--minimized"]),
         ))
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
